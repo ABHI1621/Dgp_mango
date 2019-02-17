732f68433c3b834b96bf3afcad13d917ba7e8f43
ABHISHEK JHA <abhishek.jha1831@gmail.com>
2019-02-17 10:50:59 +0530

    Dgp Mango website devloped with PHP

---
+++ index.php
@@ -0,0 +1,349 @@
+
+
+
+
+<!doctype html>
+<html lang="en">
+   <?php include_once'core/frontend/common/header.php'; ?>  
+   <body class=" ">
+        <a id="start"></a>
+       <?php include_once'core/frontend/common/navigation.php'; ?>      
+        <div class="main-container">
+          
+  <section class="cover height-80 imagebg switchable siwtchable--switch" data-overlay="8">
+                <div class="background-image-holder">
+                    <img alt="background" src="img/landing-1.jpg" />
+                </div>
+                <div class="container pos-vertical-center">
+                    <div class="row justify-content-around">
+                        <div class="col-lg-5 col-md-7">
+                        	<h1>
+                                   Join our network
+                                </h1>
+                        	<p>268 vehicles have already joined this historical moment.</p>
+                         <div class="switchable__text">
+                                        <form>
+                                    <div class="input-select">
+                                            <select  name="vehicle">
+                                            	<option value="1">Bike</option>
+                                            	<option value="2">Scooty</option>
+                                            	<option value="3">E-Bike</option>
+                                            	<option value="4">Cycle</option>
+                                            	<option value="5">Auto</option>
+                                            	<option value="6">E-Rickshaw</option>
+                                            	<option value="7">Car</option>
+                                            	<option value="8">Bus</option>
+                                            	<option value="9">Truck</option>
+                                            	<option value="10">Mini-Van</option>
+                                            	<option value="11">Mini-Truck</option>
+                                            	<option value="12">Heavy Machinery Vehicle</option>
+                                            </select>
+                                        </div>
+                                        <div  class="input" >
+                                            <input type="number" name="phone" placeholder="Enter Mobile (+917063495210)">
+                                      
+                                        </div>
+                                       <br>
+                                        <div >
+                                            <button type="button" class="btn btn--sm type--uppercase" style="color: white; width: 100%;">Next</button>
+                                        </div>
+                                 
+                                </form>
+                                <!--<a class="btn btn--primary type--uppercase" href="index.html">-->
+                                <!--    <span class="btn__text">-->
+                                <!--        View The Demos-->
+                                <!--    </span>-->
+                                <!--</a>-->
+                            </div>
+                             
+                        
+                            
+                           
+                        </div>
+                        <div class="col-lg-6 col-md-4 col-12">
+                            <div class="switchable__text">
+                                <h1>
+                                    Durgapur is changing to Mango City.
+                                </h1>
+                                <p class="lead">
+                                   Soon you can book a Bike-Taxi or Order Anything from anywhere in the city.
+                                   With or without a smart phone.<br>Give rides and earn while you change the city.
+                                </p>
+                                <!--<a class="btn btn--primary type--uppercase" href="index.html">-->
+                                <!--    <span class="btn__text">-->
+                                <!--        View The Demos-->
+                                <!--    </span>-->
+                                <!--</a>-->
+                            </div>
+                            <!--end video cover-->
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6">
+                            <h4>Popular Services</h4>
+                        </div>
+                        <div class="col-6 text-right">
+                            <a href="#">More services coming soon...</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-6">
+                            <a href="#" class="block">
+                                <div class="feature feature-7 boxed text-center imagebg" data-overlay="3">
+                                    <div class="background-image-holder" style="background: url(&quot;img/bike2.jpg&quot;); opacity: 1;">
+                                        <img alt="background" src="img/bike2.jpg">
+                                    </div>
+                                    <h4 class="pos-vertical-center">Bike Taxi</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4 col-6">
+                            <a href="#" class="block">
+                                <div class="feature feature-7 boxed text-center imagebg" data-overlay="3">
+                                    <div class="background-image-holder" style="background: url(&quot;img/orderanything.jpg&quot;); opacity: 1;">
+                                        <img alt="background" src="img/orderanything.jpg">
+                                    </div>
+                                    <h4 class="pos-vertical-center">Order Anything</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4 col-6">
+                            <a href="#" class="block">
+                                <div class="feature feature-7 boxed text-center imagebg" data-overlay="3">
+                                    <div class="background-image-holder" style="background: url(&quot;img/pickupdrop2.jpg&quot;); opacity: 1;">
+                                        <img alt="background" src="img/pickupdrop2.jpg">
+                                    </div>
+                                    <h4 class="pos-vertical-center">Anything<br>Pickup & Drop</h4>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            
+            
+     <section class="feature-large switchable bg--secondary">
+                <div class="container">
+                    <div class="row justify-content-around">
+                        <div class="col-lg-6 col-md-6">
+                            <img alt="Image" class="border--round box-shadow-wide" src="img/inner-5.jpg">
+                        </div>
+                        <div class="col-lg-5 col-md-6">
+                            <div class="heading-block">
+                                <h2>Experience quality with Stack Accomodation</h2>
+                            </div>
+                            <div class="text-block">
+                                <h5>Find unique experiences</h5>
+                                <p>
+                                    Combine blocks from a range of categories to build pages that are rich in visual style and interactivity.
+                                </p>
+                            </div>
+                            <div class="text-block">
+                                <h5>Book with confidence</h5>
+                                <p>
+                                    Stack is built with customization and ease-of-use at its core — consistent markup and useful data attribute modifiers make rapid development simple.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>       
+            
+      <section class="switchable switchable--switch feature-large">
+                <div class="container">
+                    <div class="row justify-content-around">
+                        <div class="col-md-6 col-12">
+                            <div class="video-cover border--round box-shadow-wide">
+                                <div class="background-image-holder" style="background: url(&quot;img/landing-22.jpg&quot;); opacity: 1;">
+                                    <img alt="image" src="img/landing-22.jpg">
+                                </div>
+                                <div class="video-play-icon"></div>
+                                <iframe data-src="https://www.youtube.com/embed/6p45ooZOOPo?autoplay=1" allowfullscreen="allowfullscreen"></iframe>
+                            </div>
+                            <!--end video cover-->
+                        </div>
+                        <div class="col-md-6 col-lg-5">
+                            <div class="switchable__text">
+                                <h2>Perfect components for modern startups</h2>
+                                <div class="feature feature-2 boxed">
+                                <i class="icon icon-Clock-Back color--primary"></i>
+                                <div class="feature__body">
+                                    <h5>Time Saving Components</h5>
+                                    <p>
+                                        Save time with over 290 carefully styled components designed to showcase your content
+                                    </p>
+                                </div>
+                            </div>
+                            <!--end feature-->
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            
+            
+            	
+        <section class="feature-large feature-large-2 bg--secondary">
+                <div class="container">
+                    <div class="row justify-content-around">
+                        <div class="col-md-4 col-lg-3">
+                            <h3>Experience quality</h3>
+                            <p class="lead">
+                                Stack is built with customization and ease-of-use at its core — whether you're a seasoned developer or just starting out, you'll be making attractive sites faster than any traditional HTML template.
+                            </p>
+                        </div>
+                        <div class="col-md-4 col-lg-4">
+                            <img alt="Image" class="border--round box-shadow-wide" src="img/landing-7.jpg">
+                        </div>
+                        <div class="col-md-4 col-lg-2">
+                            <hr class="short">
+                            <p>
+                                Each purchase of Stack includes six months of free support, including access to our dedicated support forum. In addition to support you'll recieve lifetime updates, including new content and bug-fixes.
+                            </p>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+        
+        
+        
+            <section class="text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2>A trusted partner to the best</h2>
+                            <p class="lead">
+                                Working with the industries best and brightest minds to deliver outstanding results
+                            </p>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            
+            <section class="text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <ul class="list-inline list-inline--images">
+                                <li>
+                                    <img alt="Image" src="img/partner-1.png">
+                                </li>
+                                <li>
+                                    <img alt="Image" src="img/partner-5.png">
+                                </li>
+                                <li>
+                                    <img alt="Image" src="img/partner-7.png">
+                                </li>
+                                <li>
+                                    <img alt="Image" src="img/partner-4.png">
+                                </li>
+                                <li>
+                                    <img alt="Image" src="img/partner-6.png">
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            
+            
+            <footer class="footer-3 text-center-xs space--xs ">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img alt="Image" class="logo" src="img/logo-dark.png" />
+                            <ul class="list-inline list--hover">
+                                <li class="list-inline-item">
+                                    <a href="#">
+                                        <span class="type--fine-print">Get Started</span>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#">
+                                        <span class="type--fine-print">help@stack.io</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 text-right text-center-xs">
+                            <ul class="social-list list-inline list--hover">
+                                <li class="list-inline-item">
+                                    <a href="#">
+                                        <i class="socicon socicon-google icon icon--xs"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#">
+                                        <i class="socicon socicon-twitter icon icon--xs"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#">
+                                        <i class="socicon socicon-facebook icon icon--xs"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#">
+                                        <i class="socicon socicon-instagram icon icon--xs"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p class="type--fine-print">
+                                Supercharge your web workflow
+                            </p>
+                        </div>
+                        <div class="col-md-6 text-right text-center-xs">
+                            <span class="type--fine-print">&copy;
+                                <span class="update-year"></span> Stack Inc.</span>
+                            <a class="type--fine-print" href="#">Privacy Policy</a>
+                            <a class="type--fine-print" href="#">Legal</a>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </footer>        </div>
+        <!--<div class="loader"></div>-->
+        <a class="back-to-top inner-link" href="#start" data-scroll-class="100vh:active">
+            <i class="stack-interface stack-up-open-big"></i>
+        </a>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/flickity.min.js"></script>
+        <script src="js/easypiechart.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/typed.min.js"></script>
+        <script src="js/datepicker.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/ytplayer.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/granim.min.js"></script>
+        <script src="js/jquery.steps.min.js"></script>
+        <script src="js/countdown.min.js"></script>
+        <script src="js/twitterfetcher.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/scripts.js"></script>    </body>
+</html>
